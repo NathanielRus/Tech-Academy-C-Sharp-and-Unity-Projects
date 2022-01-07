683e9787d3acf01bd00792413b97fcc6507d54b6
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-07 16:37:21 -0700

    Math-Comparison Operators Assignment

---
+++ Math_Comparisons/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Comparisons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            int Hrate1 = 15;
+            int Hworked1 = 40;
+            Console.WriteLine(Hrate1);
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine(Hworked1);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            int Hrate2 = 20;
+            int Hworked2 = 40;
+            Console.WriteLine(Hrate2);
+            Console.WriteLine("Hours worked per week?");
+            Console.WriteLine(Hworked2);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Annual salary of Person 1:");
+            int Annual = 2080;
+            int Asalary1 = Hrate1 * Annual;
+            Console.WriteLine(Asalary1);
+
+            Console.WriteLine("Annual salary of Person 2:");
+            int Asalary2 = Hrate2 * Annual;
+            Console.WriteLine(Asalary2);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool compareIncome = Asalary1 > Asalary2;
+            Console.WriteLine(compareIncome);
+
+            Console.ReadLine();
+        }
+    }
+}
