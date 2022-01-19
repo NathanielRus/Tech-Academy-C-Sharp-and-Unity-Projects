eaf747b7ac0907d2012706109670d64c21e609ec
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-19 15:40:24 -0700

    Calling Methods

---
+++ Calling_Methods_Assignment/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    public class Operations
+    {
+        public int Add(int userIn)
+        {
+            return userIn + 12;
+        }
+
+        public int Multiply(int Sum)
+        {
+            return Sum * 5;
+        }
+
+        public int Divide(int Mul)
+        {
+            return Mul / 4;
+        }
+    }
+}
---
+++ Calling_Methods_Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, lets do some math with methods!");
+            Console.WriteLine("Please enter a number.");
+
+            int userIn = Convert.ToInt32(Console.ReadLine());
+
+            Operations Ops = new Operations();
+
+            Console.WriteLine(userIn + " + 12 =");
+            int Sum = Ops.Add(userIn);
+            Console.WriteLine(Sum);
+            Console.ReadLine();
+
+            Console.WriteLine(Sum + " * 5 =");
+            int Mul = Ops.Multiply(Sum);
+            Console.WriteLine(Mul);
+            Console.ReadLine();
+
+            Console.WriteLine(Mul + " / 4 =");
+            int Div = Ops.Divide(Mul);
+            Console.WriteLine(Div);
+            Console.ReadLine();
+        }
+    }
+}
