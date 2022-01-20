485b17f97ffb61fe9e5c5c202a0e222bd9a55414
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-20 12:43:36 -0700

    Method2 Assignment

---
+++ Method2_Assignment/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2_Assignment
+{
+    public class Math
+    {
+        
+        public int Add(int userIn1, int userIn2)
+        {
+            int res = userIn1 * userIn2;
+            return res;
+        }
+
+        public int userIn1 { get; set; }
+        public int userIn2 { get; set; }
+    }
+}
---
+++ Method2_Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Math Ops = new Math();
+            
+
+            Console.WriteLine("Hi, please enter a number.");
+            Ops.userIn1 = Convert.ToInt32(Console.ReadLine());
+            
+
+            try
+            {
+                Console.WriteLine("Good, now enter another number. You do not have to enter a 2nd number.");
+                Ops.userIn2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You skipped the second number.");
+                Ops.userIn2 = 0;
+            }
+            int Operation = Ops.Add(Ops.userIn1, Ops.userIn2);
+            Console.WriteLine("The sum of " + Ops.userIn1 + " * " + Ops.userIn2 + " = " + Operation);
+            
+            Console.ReadLine();
+        }
+    }
+}
