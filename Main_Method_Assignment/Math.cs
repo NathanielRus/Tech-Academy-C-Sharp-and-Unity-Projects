acd0024b03492be336318ebcc111f55a294537b7
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-19 19:56:50 -0700

    Main Method Assignment

---
+++ Main_Method_Assignment/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class Math
+    {
+        public int Add(int userIn)
+        {
+            return userIn + 12;
+        }
+
+        public decimal Add(decimal decIn)
+        {
+            return decIn + 5.75M;
+        }
+
+        public int Add(string strIn, int userIn)
+        {
+            int intIn = Convert.ToInt32(strIn);
+            int newInt = intIn * userIn / 2;
+            return newInt;
+        }
+    }
+}
---
+++ Main_Method_Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, lets do some math with methods!");
+            Console.WriteLine("Please enter a number.");
+
+
+            int userIn = Convert.ToInt32(Console.ReadLine());
+
+            Math Ops = new Math();
+
+            Console.WriteLine(userIn + " + 12 =");
+            int Sum = Ops.Add(userIn);
+            Console.WriteLine(Sum);
+
+            Console.WriteLine("Good, now type in a decimal!");
+            decimal decIn = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(decIn + " + 5.75 =");
+            decimal dec = Ops.Add(decIn);
+            Console.WriteLine(dec);
+
+            Console.WriteLine("Nice, now type in another number.");
+            string strIn = Console.ReadLine();
+
+
+            Console.WriteLine(strIn + " * " + userIn + " / 2 =");
+            int newInt = Ops.Add(strIn, userIn);
+            Console.WriteLine(newInt);
+            Console.ReadLine();
+        }
+    }
+}
