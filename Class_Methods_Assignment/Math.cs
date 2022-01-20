d7699e4a5d2f2c994430465ceb77a9f2c1fa22fb
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-20 15:25:58 -0700

    Different Classes and Methods

---
+++ Class_Methods_Assignment/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Assignment
+{
+    class Math
+    {
+        public void Operations(int UserIn1)
+        {
+            Console.WriteLine(UserIn1 / 2);
+        }
+        
+        public void Operations(int DiffNum, int Hardnum)
+        {
+            Console.WriteLine(DiffNum * Hardnum);
+        }
+
+    }
+}
---
+++ Class_Methods_Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods_Assignment
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Ops = new Math();
+
+
+            Console.WriteLine("Hi, please enter a number.");
+            Ops.Operations(UserIn1: Convert.ToInt32(Console.ReadLine()));
+
+            Console.ReadLine();
+
+            Console.WriteLine("Good now enter a different number.");
+            Ops.Operations(DiffNum: Convert.ToInt32(Console.ReadLine()),Hardnum: 10);
+
+            Console.ReadLine();
+        }
+    }
+}
