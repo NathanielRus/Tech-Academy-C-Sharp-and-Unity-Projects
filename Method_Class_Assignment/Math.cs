bc3c07e584eeb7afa085b50703c40df3f2d5a67f
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-20 13:34:02 -0700

    Method Class Assignment

---
+++ Method_Class_Assignment/Math.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Math
+    {
+        public void Simple(int first = 10, int second = 45)
+        {
+            int third = first + 12;
+        }
+
+        public int third { get; set; }
+        public int second { get; set; }
+
+    }    
+}
---
+++ Method_Class_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Ops = new Math();
+
+            Console.WriteLine(Ops.third);
+            Console.WriteLine(Ops.second);
+
+            Console.ReadLine();
+        
+        }
+    }
+}
