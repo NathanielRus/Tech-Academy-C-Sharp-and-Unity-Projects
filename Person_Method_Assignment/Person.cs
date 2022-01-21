65da64d8c6ac095ca639f14248ac4a299791cce2
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-20 17:32:27 -0700

    Methods and Objects assignment.

---
+++ Person_Method_Assignment/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Method_Assignment
+{
+    public class Person
+    {
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Person_Method_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            Person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
