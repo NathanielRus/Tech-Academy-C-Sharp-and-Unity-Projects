8c2a0105afa24426843dee1ccf5b6ed112b31ea1
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-21 14:46:52 -0700

    Abstract Class Assignment

---
+++ Abstract_Class_Assignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Abstract_Class_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            Employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
