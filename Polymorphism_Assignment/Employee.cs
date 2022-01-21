714dfcbc787482f9759efbd73dfeba77371d3c90
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-21 16:06:17 -0700

    Polymorphism Assignment

---
+++ Polymorphism_Assignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment
+{
+    public class Employee : Person, IQuittbale
+    {
+        public new void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName + " " + Years);
+        }
+
+        public int Years { get; set; }
+    }
+}
---
+++ Polymorphism_Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            Employee.SayName();
+
+            IQuittbale person = new Employee() { FirstName = "Sample", LastName = "Student", Years = 5 };
+            person.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
