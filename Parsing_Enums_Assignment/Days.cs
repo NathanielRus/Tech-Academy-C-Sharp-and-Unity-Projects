3c42b1f73085b07775f5b8eb3978bc1f135d84be
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-24 16:16:58 -0700

    Parsing Enums

---
+++ Parsing_Enums_Assignment/Days.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    public class Days
+    {
+        public DayOfWeek InDay { get; set; }
+    }
+
+    public enum DayOfWeek
+    {
+        None,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ Parsing_Enums_Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parsing_Enums_Assignment
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DayOfWeek days;
+            Console.WriteLine("Please enter in the current day of the week.");
+            string InDay = Console.ReadLine();
+
+            try
+            {
+                days = (DayOfWeek)Enum.Parse(typeof(DayOfWeek),InDay);
+                Console.WriteLine("Today is: " + InDay);
+
+            }
+
+
+            catch (Exception)
+            {
+
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+
+        }     
+    }
+}
