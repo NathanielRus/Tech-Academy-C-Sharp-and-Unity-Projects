05aefe1fb853ea4a11d52a3686395b7b322df2ac
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-24 14:17:32 -0700

    Generic Assignment

---
+++ Parameters_Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string>() { "Joe", "Bob", "Sue", "Kate" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>() { 2001, 2002, 2003, 2004 };
+
+
+            for (int i = 0; i < employee2.Things.Count(); i++)
+            {
+                Console.WriteLine(employee1.Things[i]);
+                Console.WriteLine(employee2.Things[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
