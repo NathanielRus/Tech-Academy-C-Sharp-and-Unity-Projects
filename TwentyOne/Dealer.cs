eb271c35d957b343bc71852dea9c569c12518582
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-26 18:53:20 -0700

    Added time stamp to log

--- TwentyOne/Dealer.cs
+++ TwentyOne/Dealer.cs
@@ -20,6 +20,7 @@ namespace TwentyOne
             Console.WriteLine(card);
             using (StreamWriter file = new StreamWriter(@"C:\Users\3scor\Documents\GitHub\Tech-Academy-C-Sharp-and-Unity-Projects\TwentyOne\Logs\log.txt", true))
             {
+                file.WriteLine(DateTime.Now);
                 file.WriteLine(card);
             }
             Deck.Cards.RemoveAt(0);
