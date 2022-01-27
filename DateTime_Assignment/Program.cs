c2e613102867e4eb7218bca3aac5d96d77a685fa
Nathaniel R <94143230+NathanielRus@users.noreply.github.com>
2022-01-26 19:22:02 -0700

    DateTime Assignment

---
+++ DateTime_Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            DateTime currentTime = DateTime.Now;
+
+            Console.WriteLine("Please enter a number to add hours onto the current time.");
+            DateTime userHourLater = currentTime.AddHours(Convert.ToDouble(Console.ReadLine()));
+            
+                        
+
+            Console.WriteLine(string.Format("{0} Will be... {1}", currentTime, userHourLater));
+
+            Console.ReadLine();
+        }
+    }
+}
